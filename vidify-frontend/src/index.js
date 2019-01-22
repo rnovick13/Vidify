7535c67355e906f40ec3bbdc918c029c0983f13e
github username <github email address>
2019-01-22 18:26:03 -0500

    rendering playlists

--- vidify-frontend/src/index.js
+++ vidify-frontend/src/index.js
@@ -1,14 +1,15 @@
 document.addEventListener('DOMContentLoaded', ()=>{
   fetch('http://localhost:3000/playlists/')
   .then(res => res.json())
-  .then(playlist => { renderPlaylist(playlist)
+  .then(playlists => {
+    renderPlaylists(playlists)
   })
 })
 
-function renderPlaylist(playlist) {
-    let headerTag = document.querySelector('name')
-    playlist.forEach(playlist => {renderPlaylist(playlist)})
-    // debugger
-    headerTag.innerText = `${playlist.name}`
+function renderPlaylists(playlists) {
+    let headerTag = document.querySelector('#name')
+    // playlist.forEach(playlist => {renderPlaylist(playlist)})
+    headerTag.innerText = `${playlists.name}`
+    debugger
     headerTag.classList.add('playlist')
 }
