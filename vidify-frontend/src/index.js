6d4c1b270635f63f4ccae7c9fad780df652ea3fb
rnovick13 <rnovick13@gmail.com>
2019-01-22 16:39:55 -0500

    JS work

--- vidify-frontend/src/index.js
+++ vidify-frontend/src/index.js
@@ -1,5 +1,14 @@
 document.addEventListener('DOMContentLoaded', ()=>{
   fetch('http://localhost:3000/playlists/')
   .then(res => res.json())
-  .then(json => console.log(json));
+  .then(playlist => { renderPlaylist(playlist)
+  })
 })
+
+function renderPlaylist(playlist) {
+    let headerTag = document.querySelector('name')
+    playlist.forEach(playlist => {renderPlaylist(playlist)})
+    // debugger
+    headerTag.innerText = `${playlist.name}`
+    headerTag.classList.add('playlist')
+}
