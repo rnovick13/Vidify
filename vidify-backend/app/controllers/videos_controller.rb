5a96b9d40507e14f9908cce4861025c37b3d04b5
github username <github email address>
2019-01-22 11:38:06 -0500

    added seed data

--- vidify-backend/app/controllers/videos_controller.rb
+++ vidify-backend/app/controllers/videos_controller.rb
@@ -1,7 +1,11 @@
 class VideosController < ApplicationController
   def index
+    @videos = Video.all
+    render json: @videos
   end
 
   def show
+    @video = Video.find(params[:id])
+    render json: @video
   end
 end
