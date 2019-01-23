377ed18e066e9b80fb45e946c2aa016a2a286518
rnovick13 <rnovick13@gmail.com>
2019-01-23 17:47:15 -0500

    worked on view playlist button/route

--- vidify-frontend/src/index.js
+++ vidify-frontend/src/index.js
@@ -5,7 +5,6 @@ document.addEventListener('DOMContentLoaded', ()=>{
     renderPlaylists(playlists)
   })
 })
-  // debugger
 
 
 function renderPlaylists(playlists) {
@@ -16,16 +15,46 @@ function renderPlaylists(playlists) {
 
 function playlistInfo(playlist) {
   let playlistDiv = document.querySelector('.playlist')
-  const headerTag = document.createElement('h2')
-  const imgTag = document.createElement('img')
 
+  const headerTag = document.createElement('h2')
   headerTag.innerText = `${playlist.name}`
   headerTag.classList.add('card')
+
+  const imgTag = document.createElement('img')
   imgTag.src=`${playlist.source}`
   imgTag.classList.add('card')
 
+  const viewButton = document.createElement('button')
+  viewButton.innerText = "View Playlist"
+  viewButton.id = `view-${playlist.id}`
+  viewButton.addEventListener('click', viewPlaylist)
+
   playlistDiv.appendChild(headerTag)
   playlistDiv.appendChild(imgTag)
+  playlistDiv.appendChild(viewButton)
+}
+
+function viewPlaylist(e){
+  let id = e.target.id.split('-')[1]
+  document.open()
+  renderPlaylistVideos(id)
+}
+
+//potentially just remove div class="playlist" instead of document.open()
+//we have the id
+//fetch the playlist videos show page
+//render the data on the web page
+
+
+
+function renderPlaylistVideos(id){
+  const playlistName = document.createElement('h1')
+  // const playlist =
+  // debugger
+  // playlistName.innerText =
+  //playlist name as header
+  //list video names
+  //video names clickable links to actual videos
 }
 
 
