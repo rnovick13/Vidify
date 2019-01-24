f275ca7f7e95c22036d7b041b3c893f959bb99a5
rnovick13 <rnovick13@gmail.com>
2019-01-24 16:00:47 -0500

    cards work

--- vidify-frontend/src/index.js
+++ vidify-frontend/src/index.js
@@ -12,6 +12,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
       playlistFormDiv().style.display = 'none'
     }
   })
+  // replaceDOM()
 })
 
 function resetForm() {
@@ -33,42 +34,92 @@ function renderPlaylists(playlists) {
 }
 
 function playlistInfo(playlist) {
-  let playlistDiv = document.querySelector('.playlist')
+  let playlistDiv = document.querySelector('.playlist') //grandparent
+
+  // playlistDiv.classList.add('card')
+  //make mommy or daddy aka div
+  let cardDiv = document.createElement('div')
+  cardDiv.classList.add('card')
 
   const headerTag = document.createElement('h2')
   headerTag.innerText = `${playlist.name}`
-  headerTag.classList.add('card')
+  headerTag.classList.add('playlist')
+  // headerTag.classList.add('card')
 
   const imgTag = document.createElement('img')
   imgTag.src=`${playlist.source}`
-  imgTag.classList.add('card')
+  imgTag.classList.add('playlist')
+  // imgTag.classList.add('card')
 
   const viewButton = document.createElement('button')
   viewButton.innerText = "View Playlist"
   viewButton.id = `view-${playlist.id}`
+  viewButton.classList.add('playlist')
+  // viewButton.classList.add('card')
   viewButton.addEventListener('click', viewPlaylist)
 
-  playlistDiv.appendChild(headerTag)
-  playlistDiv.appendChild(imgTag)
-  playlistDiv.appendChild(viewButton)
+  cardDiv.appendChild(headerTag)
+  cardDiv.appendChild(imgTag)
+  cardDiv.appendChild(viewButton)
+  playlistDiv.appendChild(cardDiv)
 }
-
-function viewPlaylist(e){
-  let id = e.target.id.split('-')[1]
-  // document.open()
-  // renderPlaylistVideos(id)
-}
-
 //potentially just remove div class="playlist" instead of document.open()
 //we have the id
 //fetch the playlist videos show page
 //render the data on the web page
 
+function replaceDOM() {
+  let body = document.querySelector(".site-body")
+  body.innerHTML = ""
+
+  // let home = document.querySelector(".playlist")
+  let newPage = document.querySelector(".video-list")
+  body.appendChild(newPage)
 
 
-function renderPlaylistVideos(id){
-  const playlistName = document.createElement('h1')
+}
+
+function viewPlaylist(e){
+  let id = e.target.id.split('-')[1]
+  // document.open()
+  //remove div class "playlist"
+  getVideos(id)
+  // renderPlaylistVideos(id)
+}
+
+function getVideos(id) {
+  fetch(`http://localhost:3000/playlists/${id}`)
+  .then(res => res.json())
+  .then(data => {
+    renderPlaylistVideos(data)
+  })
+}
+
+function renderPlaylistVideos(data){
+  let videoPage = document.querySelector('.video-list')
+
+  const playlistName = document.createElement('h2')
+  playlistName.innerText = data.name
+  // debugger
+  videoPage.appendChild(playlistName)
   //playlist name as header
+
+  data.videos.forEach(video => {
+    const videoList = document.createElement('ul')
+    const videos = document.createElement('li')
+    videos.innerText = `${video.name}`
+    videoList.appendChild(videos)
+
+    const watchButton = document.createElement('button')
+    watchButton.innerText = "Watch Video"
+    // watchButton.addEventListener('click', linkToVideo(video.source))
+    // console.log("Hits")
+    })
+
+    function linkToVideo(videoSource) {
+
+    }
+
   //list video names
   //video names clickable links to actual videos
 }
